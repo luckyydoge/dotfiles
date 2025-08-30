8ab5702fa78350135c7834b660eb8762b1a70ef5
zjw <zjw000107@gmail.com>
2025-08-30 13:07:16 +0800

    nvim修改tab为空格，tab默认4格

--- nvim/.config/nvim/lua/core/options.lua
+++ nvim/.config/nvim/lua/core/options.lua
@@ -30,6 +30,10 @@ vim.o.splitbelow = true
 vim.o.list = true
 vim.opt.listchars = { tab = '>-', trail = '·', nbsp = '␣' }
 
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
 vim.o.inccommand = 'split'
 
 vim.o.cursorline = true
